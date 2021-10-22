378e7380abcfd56d94949e7dc1d6adc97755a7ab
S-011 <u201811286@upc.edu.pe>
2021-10-22 00:36:54 -0500

    fix: deleted unused components
    Some unused components were deleted.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,7 @@ import { BodyComponent } from './pages/body/body.component';
 import { FooterComponent } from './pages/footer/footer.component';
 import { HeaderComponent } from './pages/header/header.component';
 import {NutritionistsComponent} from "./pages/nutritionists/pages/nutritionists/nutritionists.component";
-import {AppoimentComponent} from "./appoiment/appoiment.component";
 import {ClientsComponent} from "./pages/clients/pages/clients/clients.component";
-import {DialogCreateClientsComponent} from "./pages/clients/pages/dialog-create-clients/dialog-create-clients.component";
 import {RecipesComponent} from "./pages/recipe/pages/recipe/recipes.component";
 import { DialogAddRecipeComponent } from './pages/recipe/pages/dialog-add-recipe/dialog-add-recipe.component';
 import { DietsComponent } from './pages/diets/pages/diets/diets.component';
@@ -27,9 +25,7 @@ import { AppointmentsComponent } from './pages/appointments/pages/appointments/a
     FooterComponent,
     HeaderComponent,
     NutritionistsComponent,
-    AppoimentComponent,
     ClientsComponent,
-    DialogCreateClientsComponent,
     RecipesComponent,
     DialogAddRecipeComponent,
     DietsComponent,
